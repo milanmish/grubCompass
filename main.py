4e6b5485b3a39b53c7ce0b3b7ea54979e1af95e9
Milan Mishra <milanmishr20@gmail.com>
2025-05-22 16:51:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import serial
+import requests
+import random
+import time
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 api_key = os.getenv("GOOGLE_API_KEY")
+#BAUD = 9600
+#PORT = 
 
+def get_nearby_restaurants(lat, lon):
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    params = {
+        "location": f"{lat},{lon}",
+        "radius": 2000,
+        "type": "restaurant",
+        "open_now": True,
+        "key": api_key
+    }
+    response = requests.get(url, params=params)
+    return response.json().get("results", [])
+
+print(get_nearby_restaurants(44.963735, -93.308424))
